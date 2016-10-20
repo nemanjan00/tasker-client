d792b8bd4cad352c7d9d632bdd7425fd905daf2c
Nemanja Nedeljković <nemanjan00@gmail.com>
2016-10-20 09:25:07 +0200

    Init

--- example.js
+++ example.js
@@ -1,5 +1,18 @@
-var client = require("./index")();
+const exec = require('child_process').exec;
+
+var client = require("./index")({});
 
 client.onMessage(function(message){
-	console.log(message);
+	if(message.action == "nextSong"){
+		exec('playerctl next', (err, stdout, stderr) => {});
+	}
+
+	if(message.action == "pauseSong"){
+		exec('playerctl pause', (err, stdout, stderr) => {});
+	}
+
+	if(message.action == "playSong"){
+		exec('playerctl play', (err, stdout, stderr) => {});
+	}
 });
+
